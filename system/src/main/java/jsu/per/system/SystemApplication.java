4fc3ba91dbc3113921ce2441e0076e60011f0e8f
无敌的程序猿 <1987888060@qq.com>
2022-03-22 21:02:29 +0800

    2022-3-22 登陆功能01

--- system/src/main/java/jsu/per/system/SystemApplication.java
+++ system/src/main/java/jsu/per/system/SystemApplication.java
@@ -1,9 +1,12 @@
 package jsu.per.system;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("jsu.per.system.dao")
 public class SystemApplication {
 
     public static void main(String[] args) {
--- system/src/main/java/jsu/per/system/dao/RoleMapper.java
+++ system/src/main/java/jsu/per/system/dao/RoleMapper.java
@@ -4,6 +4,9 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import jsu.per.system.pojo.Role;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface RoleMapper extends BaseMapper<Role> {
+    List<Integer> findRight(int id);
 }
